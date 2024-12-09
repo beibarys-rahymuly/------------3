a7f6cf89081fd8a6279fe8be78e175b22e9f509b
beibarys <tvbeiba@gmail.com>
2024-12-09 11:07:34 +0300

    ++

--- 4/5/js/script.js
+++ 4/5/js/script.js
@@ -1,12 +1,75 @@
 
+// var isRain = prompt('сейчас дождь?')
 
-let name ="Beibarys";
-let surname = "Beiba";
-let age = 17;
-let = birithDay = "22.11.2007";
-let school = "210 litshei"
-console.log("Aty", name); 
-console.log("Tegi", surname ); 
-console.log("Zhasy", age); 
-console.log("Tuylgan kuni", birithDay);
+// if (isRain == 'да' || isRain=='да') {
+//     alert('тогда иди с машиной'); 
+// }   else if (isRain == 'нет') {
+// var isMorning = prompt('сейчас утро?')
+// }
+
+// if (isRain == 'нет' || isRain== 'нет') {
+//     alert('тогда иди с мотиком');
+// }   else if (isRain == 'да') {
+//     var isMorning = prompt('сейчас обед?')
+// }   if (isMorning == "да") {
+//     alert ("тогда приятного апетита!")
+// }   else if (isMorning == "нет") {
+//     alert ("иди кушай тогда.")
+// }
+
+
+//var  isName = prompt ('атыныз кым?')
+//var isTanda = prompt ('тагамга тапсырыс берыныз' + '\n' + 'А) Бургер' + '/n' + 'Б) Донер' + '/n' + 'c) Фри соусы мен')
+
+//if (isTanda == 'A' || isTanda== 'a') {
+  //  alert ('Бургер')
+//} else if (isTanda == Б || isTanda == б )
+  //  alert ('Донер')
+
+//var herName = prompt("Есіміңіз кім ?");
+// var isOrder = prompt("Бургер тапсырыс беріңіз" + "\n" + "А)Куринный" + "\n" + "Б)Говяжий" + "\n" + "С)Етсіз");
+// var meet = ""
+
+// if (isOrder == "а" || isOrder == "А") {
+    // var meet = "Куринный";
+// } else if (isOrder == "б" || isOrder == "Б") {
+    // var meet = "Говяжий";
+// } else if (isOrder == "с" || isOrder == "С") {
+    // var meet = "Етсіз";
+// }
+
+// var isOrder2 = prompt("Сусын тапсырыс беріңіз" + "\n" + "А)Кола" + "\n" + "Б)Спрайт" + "\n" + "С)Фанта");
+// var water = ""
+
+// if (isOrder2 == "а" || isOrder == "А") {
+    // var water = "Кола";
+// } else if (isOrder == "б" || isOrder == "Б") {
+    // var water = "Спрайт";
+// } else if (isOrder == "с" || isOrder == "С") {
+    var water = "Фанта";
+}
+
+var isOrder3 = prompt("Фри тапсырыс беріңіз" + "\n" + "А)Кіш" + "\n" + "Б)Орт" + "\n" + "С)Үлк");
+var fri = ""
+
+if (isOrder3 == "а" || isOrder == "А") {
+    var fri = "Кіш";
+} else if (isOrder == "б" || isOrder == "Б") {
+    var fri = "Орт";
+} else if (isOrder == "с" || isOrder == "С") {
+    var fri = "Үлк";
+}
+
+var isOrder = prompt("Соус таңдаңыз" + "\n" + "А)BBQ" + "\n" + "Б)Cheese" + "\n" + "С)Ketchup");
+var souce = ""
+
+if (isOrder == "а" || isOrder == "А") {
+    var souce = "BBQ";
+} else if (isOrder == "б" || isOrder == "Б") {
+    var souce = "Cheese";
+} else if (isOrder == "с" || isOrder == "С") {
+    var souce = "Ketchup";
+}
+
+alert("Бургер " + meet + "\n" + "Напиток " + water + "\n" + "Фри " + fri + "\n" + "Соус " + souce + "\n" + "Клиент аты " + herName)
 
---
+++ 6/script.js
@@ -0,0 +1,20 @@
+//const shop = {
+  //  phoneNumber: '8 707 606 70 07'
+    //products:[
+      //  {productName: 'Алма', category ['жемыс-жидек'] ,price:1500 , stock:130}
+        //{productName: 'Апельсин', category ['жемыс-жидек'] ,price:2000 , stock:160}
+        //{productName: 'алмурт', category ['жемыс-жидек'] ,price:1000 , stock:145}
+        //{productName: 'алмурт', category ['жемыс-жидек'] ,price:500 , stock:130}
+    //]
+//}
+
+const shop = {
+    this Name = newgameshop
+}
+ch]angeShopName(shopgaming)
+co
+
+
+
+
+
