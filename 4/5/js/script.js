73a42f20827b546a6a3896ec4ef0f540bb94c217
beibarys <tvbeiba@gmail.com>
2024-12-09 11:06:53 +0300

    экзамен

---
+++ 4/5/js/script.js
@@ -0,0 +1,12 @@
+
+
+let name ="Beibarys";
+let surname = "Beiba";
+let age = 17;
+let = birithDay = "22.11.2007";
+let school = "210 litshei"
+console.log("Aty", name); 
+console.log("Tegi", surname ); 
+console.log("Zhasy", age); 
+console.log("Tuylgan kuni", birithDay);
+
